9a78949d6525fe67ba2d34d45e1b9e3a96fe3837
sacca97 <tommaso.sacchetti@gmail.com>
2025-07-13 12:41:24 +0200

    update rrcosfilter

--- commpy/filters.py
+++ commpy/filters.py
@@ -118,6 +118,55 @@ def rrcosfilter(N, alpha, Ts, Fs):
 
     return time_idx, h_rrc
 
+def rrcosfilter_v2(N, alpha, Ts, Fs):
+    """
+    Generates a root raised cosine (RRC) filter (FIR) impulse response.
+
+    Parameters
+    ----------
+    N : int
+        Length of the filter in samples.
+
+    alpha : float
+        Roll off factor (Valid values are [0, 1]).
+
+    Ts : float
+        Symbol period in seconds.
+
+    Fs : float
+        Sampling Rate in Hz.
+
+    Returns
+    ---------
+
+    time_idx : 1-D ndarray of floats
+        Array containing the time indices, in seconds, for
+        the impulse response.
+
+    h_rrc : 1-D ndarray of floats
+        Impulse response of the root raised cosine filter.
+    """
+
+    T_delta = 1/float(Fs)
+    time_idx = ((np.arange(N)-N/2))*T_delta
+    sample_num = np.arange(N)
+    h_rrc = np.zeros(N, dtype=float)
+
+    for x in sample_num:
+        t = (x-N/2)*T_delta
+        if t == 0.0:
+            h_rrc[x] = (1.0/np.sqrt(Ts)) * (1.0 - alpha + (4*alpha/np.pi))
+        elif alpha != 0 and abs(t) == Ts/(4*alpha):
+            h_rrc[x] = (alpha/np.sqrt(2*Ts)) * (((1+2/np.pi) * \
+                    (np.sin(np.pi/(4*alpha)))) + ((1-2/np.pi)*(np.cos(np.pi/(4*alpha)))))
+        else:
+            numerator = np.sin(np.pi*t*(1-alpha)/Ts) + \
+                       4*alpha*(t/Ts)*np.cos(np.pi*t*(1+alpha)/Ts)
+            denominator = np.pi*t*(1-(4*alpha*t/Ts)**2)/Ts
+            h_rrc[x] = (1.0/np.sqrt(Ts)) * (numerator/denominator)
+
+    return time_idx, h_rrc
+
 def gaussianfilter(N, alpha, Ts, Fs):
     """
     Generates a gaussian filter (FIR) impulse response.
